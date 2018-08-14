b39ebb2d64e787fe18464a0135785dd10c33c1bc
damiaNR10 <dymon10@o2.pl>
2018-08-14 11:09:14 +0200

    next_commit

--- app/src/main/java/com/example/damia/colorpaletteeduweb/ColorActivity.java
+++ app/src/main/java/com/example/damia/colorpaletteeduweb/ColorActivity.java
@@ -1,13 +1,68 @@
 package com.example.damia.colorpaletteeduweb;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import static com.example.damia.colorpaletteeduweb.PaletteActivity.LOG_TAG;
 
 public class ColorActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = ColorActivity.class.getSimpleName();
+    private ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        Log.d(LOG_TAG,"onCreate()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG,"onDestroy()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG,"onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"onStop()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"onPause()");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home)
+        {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/damia/colorpaletteeduweb/PaletteActivity.java
+++ app/src/main/java/com/example/damia/colorpaletteeduweb/PaletteActivity.java
@@ -6,11 +6,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class PaletteActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = PaletteActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,37 @@ public class PaletteActivity extends AppCompatActivity {
                 addColor();
             }
         });
+        Log.d(LOG_TAG,"onCreate()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG,"onDestroy()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG,"onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"onStop()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"onPause()");
     }
 
     private void addColor() {
