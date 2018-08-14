88eca5a1f779071fc27d7e7789ef1310eddbe615
damiaNR10 <dymon10@o2.pl>
2018-08-14 14:50:23 +0200

    commit
    random color generator (for activity's background)

--- app/src/main/java/com/example/damia/colorpaletteeduweb/ColorActivity.java
+++ app/src/main/java/com/example/damia/colorpaletteeduweb/ColorActivity.java
@@ -1,68 +1,87 @@
 package com.example.damia.colorpaletteeduweb;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
-import static com.example.damia.colorpaletteeduweb.PaletteActivity.LOG_TAG;
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ColorActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = ColorActivity.class.getSimpleName();
+    @BindView(R.id.redSeekBar)
+    SeekBar redSeekBar;
+    @BindView(R.id.greenSeekBar)
+    SeekBar greenSeekBar;
+    @BindView(R.id.blueSeekBar)
+    SeekBar blueSeekBar;
+    @BindView(R.id.generateButton)
+    Button generateButton;
+    @BindView(R.id.saveButton)
+    Button saveButton;
+    @BindView(R.id.colorLinearLayout)
+    LinearLayout colorLinearLayout;
+
+
+    private int red;
+    private int green;
+    private int blue;
+
     private ActionBar actionBar;
+    private Random random = new Random(); //do generowania liczb pseudolosowych
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+        ButterKnife.bind(this);
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        Log.d(LOG_TAG,"onCreate()");
+        Log.d("sprawdzam", "sprawdzam");
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG,"onDestroy()");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG,"onStart()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG,"onStop()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG,"onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG,"onPause()");
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId() == android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             NavUtils.navigateUpFromSameTask(this);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    @OnClick(R.id.generateButton)
+    public void generate() {
+        Log.d("sprawdzam", "sprawdzam1");
+        red = random.nextInt(256); //wygenerowanie losowej wartości z przedziału 0-255
+        green = random.nextInt(256);
+        blue = random.nextInt(256);
+        Log.d("sprawdzam", "sprawdzam2");
+
+        int color = Color.rgb(red, green, blue); //"tworzenie" koloru
+        colorLinearLayout.setBackgroundColor(color); //ustawienie koloru tła
+        Log.d("sprawdzam", "sprawdzam3");
+    }
+
+    @OnClick(R.id.saveButton)
+    public void save() {
+
+    }
+
+
 }
